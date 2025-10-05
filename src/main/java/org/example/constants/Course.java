5a7c533446b926053f4248e23a445ff97bd5ecfc
Veronika <37775859+veeroosh@users.noreply.github.com>
2025-10-05 20:23:09 +0300

    Update Course.java

--- src/main/java/org/example/constants/Course.java
+++ src/main/java/org/example/constants/Course.java
@@ -6,8 +6,7 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum Course {
-  C_SHARP_DEVELOPER_PRO("C# Developer. Professional"),
-  ARCHITECTURE("Архитектура и шаблоны проектирования"),
+  DEV_OPS_PRACTICES_INSTRUMENTS("DevOps практики и инструменты"),
   DEV_OPS_ADVANCED("DevOps Advanced"),
   JAVA_QA_PRO("Java QA Engineer. Basic");
 
