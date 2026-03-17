b5a78f935715337e1de16db30a537c5d90723c50
latysheva <latysheva@protei.ru>
2026-03-17 22:03:23 +0300

    ui_tests.groovy updated

--- ui_tests.groovy
+++ ui_tests.groovy
@@ -9,7 +9,7 @@ timeout("1200") {
             currentBuild.description = "User: ${env.BUILD_USER}"
         }
 
-        def yamlConfig = readTrustedYaml text: "{$CONFIG}"
+        def yamlConfig = readYaml text: "${CONFIG}"
         sh "mkdir -p ./config"
 
         stage("Create env file") {
